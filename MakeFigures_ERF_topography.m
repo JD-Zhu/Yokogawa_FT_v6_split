f2b3fb52ada65aa895b0ee7efeb44d21bf8bf088
MS Di Zhu <43606024@students.mq.edu.au>
2018-07-05 15:32:03 +1000

    MakeFigures: misc changes to font size.

--- MakeFigures_ERF_topography.m
+++ MakeFigures_ERF_topography.m
@@ -28,17 +28,18 @@ cfg.xlim = [0.425 0.550]; % duration of the effect (as reported by ft_clusterplo
                           % topography will be averaged over this interval
 
 cfg.highlight         = 'on'; % highlight significant channels
-cfg.highlightsymbol = '+';
-cfg.highlightfontsize = 24;
+cfg.highlightsymbol = '.';
+cfg.highlightsize = 26;
 cfg.highlightcolor = 'w';
-miny = 98; % index of the time point you want (485ms - middle of the effect interval)
+miny = 98; % index of the time point you want (we selected 485ms - middle of the effect interval)
 cfg.highlightchannel  = stat.label(ismember(stat.negclusterslabelmat(:,miny),1)); % find all the channels showing '1' at that time point
 
 cfg.style             = 'straight';
 cfg.comment           = 'no';
 cfg.gridscale         = 512;
-cfg.marker            = 'off';
+cfg.marker            = 'off'; % show the location of all channels?
 
 cfg.parameter = 'stat'; % what field (in the stats output) to plot, e.g. selecting 'stat' will plot t-values
 
 figure; ft_topoplotER(cfg, stat);
+set(gca,'fontsize',22); % set colorbar fontsize
