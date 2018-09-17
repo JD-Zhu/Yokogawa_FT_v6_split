68996b9d21662a64bf6872efd1837e628cf3c449
MS Di Zhu <43606024@students.mq.edu.au>
2018-09-17 14:41:31 +1000

    Added header which explains this script.

--- MakeFigures_ERF_topography.m
+++ MakeFigures_ERF_topography.m
@@ -1,3 +1,26 @@
+%{
+Plot topography showing location of the sensors which formed the cluster.
+
+ft_clusterplot is convenient for early visualisation of significant channel 
+locations. It is a wrapper around ft_topoplot, and automatically extracts
+the spatial and temporal extent of the cluster from the stat output. It 
+produces a series of subplots, spanning the time window in which the effect
+was found (1 subplot per sample).
+
+If you do not wish to include all the subplots in your paper, you can produce 
+a plot averaged over time points (i.e. this script). You need to make the 
+decision on how to include the spatial extent (i.e. electrodes) of your
+cluster. You could show all electrodes that were part of the cluster at any
+one time point, or you could plot the electrode size as a function of how
+often (i.e. at how many time points) the electrode was part of the cluster.
+
+Here we plot the topography averaged over the interval of the effect (425-550ms), 
+and highlight the channels which are significant at the "middle" time sample (485ms).
+
+See here for more information:
+http://www.fieldtriptoolbox.org/faq/how_not_to_interpret_results_from_a_cluster-based_permutation_test
+%}
+
 %% run the #define section to obtain values for global vars
 global ResultsFolder; 
 common();
